494f50ae810ef8f14278fa542af9badf9f8b3920
Ali Hassan <hassanaliabbasi478@gmail.com>
2026-05-27 15:53:22 +0500

    Fix PostgreSQL parsing

--- restaurant_management_system/restaurant_management_system/settings.py
+++ restaurant_management_system/restaurant_management_system/settings.py
@@ -96,8 +96,10 @@ WSGI_APPLICATION = 'restaurant_management_system.wsgi.application'
  
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        config('DATABASE_URL')
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
